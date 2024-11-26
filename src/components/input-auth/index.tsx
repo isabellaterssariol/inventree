42ad7a0fc87022082ccead166767ebc1a9930bce
Isabella Terssariol <isabellagama2906@gmail.com>
2024-11-26 14:44:37 -0300

    feat: register user

--- src/components/input-auth/index.tsx
+++ src/components/input-auth/index.tsx
@@ -1,46 +1,34 @@
-import React from "react";
-
-interface InputAuthProps {
-  label?: string;
-  type?: string;
-  name?: string;
-  value?: string;
-  placeholder?: string;
-  onChange?: (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => void;
-  onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
-  error?: string;
-  className?: string;
-}
+import { InputAuthProps } from "./types";
 
 export default function InputAuth({
   label,
   type,
   name,
-  value,
   placeholder,
-  onChange,
-  onKeyDown,
   error,
   className,
+  register,
 }: InputAuthProps) {
   return (
     <div className={`flex flex-col w-full ${className}`}>
-    <label htmlFor={name} className="text-secondary mb-2">
-      {label}
-    </label>
-    <input
-      type={type}
-      id={name}
-      name={name}
-      value={value}
-      placeholder={placeholder}
-      onChange={onChange}
-      onKeyDown={onKeyDown}
-      className="p-3 border-2 border-primary-dark rounded-lg focus:outline-none focus:ring-2 focus:ring-primary transition duration-300"
-    />
-    {error && <span className="text-secondary text-sm mt-1">{error}</span>}
-  </div>
+      {label && (
+        <label htmlFor={name} className="text-secondary mb-2">
+          {label}
+        </label>
+      )}
+
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        className={`p-3 border-2 rounded-lg focus:outline-none focus:ring-2 transition duration-300 ${
+          error ? "border-red-500" : "border-primary-dark"
+        }`}
+        {...register(name)}
+      />
+
+      {error && <span className="text-red-500 text-sm mt-1">{error}</span>}
+    </div>
   );
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,60 +1,54 @@
-import React from "react";
-import InputAuth from "../../components/input-auth";
+import { useNavigate } from "react-router-dom";
+import InputAuth from "../../components/input-auth/index";
+import { useAuth } from "../../context/auth-firebase";
+import AuthForm from "../../components/auth-form";
+import { loginSchema } from "../../utils/validation";
 
 export default function Login() {
+  const { loginWithEmail, loading, error } = useAuth();
+  const navigate = useNavigate();
+
+  const onSubmit = async (data: { email: string; password: string }) => {
+    const { email, password } = data;
+    await loginWithEmail(email, password);
+    if (!error) {
+      navigate("/");
+    }
+  };
+
   return (
-    <div
-      className="relative flex items-center justify-center min-h-screen bg-cover bg-center"
-      style={{
-        backgroundImage: "url('../../assets/login_image.png')",
-      }}
+    <AuthForm
+      schema={loginSchema}
+      onSubmit={onSubmit}
+      loading={loading}
+      error={error}
+      title="Sign In"
+      description="Don't have an account?"
+      redirect="/register"
+      signInOrUp="Sign Up"
     >
-      <div className="relative z-10 w-full max-w-sm p-8 bg-gray-100 rounded-lg shadow-lg">
-        <form className="bg-primary-light p-8 rounded-lg shadow-lg max-w-sm mx-auto">
+      {(register, errors) => (
+        <>
           <InputAuth
             label="E-mail"
             type="text"
-            name="email"
             placeholder="Enter your e-mail"
             className="mb-4"
+            register={register}
+            name="email"
+            error={errors.email?.message}
           />
-
           <InputAuth
             label="Password"
             type="password"
-            name="password"
             placeholder="Enter your password"
             className="mb-6"
+            register={register}
+            name="password"
+            error={errors.password?.message}
           />
-
-          <button className="w-full py-3 bg-primary text-white rounded-lg hover:bg-primary-dark transition duration-300">
-            Sign In
-          </button>
-        </form>
-
-        <p className="flex items-center text-center my-6 text-secondary">
-          <span className="flex-grow border-t border-gray-300 mx-2"></span>
-          <span>Or</span>
-          <span className="flex-grow border-t border-gray-300 mx-2"></span>
-        </p>
-
-        <div className="flex justify-center">
-          <button className="w-13 h-13 bg-white border-none rounded-lg m-10 mx-3 cursor-pointer shadow-md hover:shadow-lg transition duration-300">
-            <img src="../../assets/facebook_auth.png" alt="Facebook Icon" />
-          </button>
-
-          <button className="w-13 h-13 bg-white border-none rounded-lg m-10 mx-3 cursor-pointer shadow-md hover:shadow-lg transition duration-300">
-            <img src="../../assets/google_auth.png" alt="Google Icon" />
-          </button>
-        </div>
-
-        <p className="text-center text-secondary">
-          Didn't have an account?{" "}
-          <a href="/" className="text-primary font-bold hover:underline">
-            Sign Up
-          </a>
-        </p>
-      </div>
-    </div>
+        </>
+      )}
+    </AuthForm>
   );
 }
