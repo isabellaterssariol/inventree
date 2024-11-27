8e8f903fc79a5596fea6f320e2798dd5def3442e
Isabella Terssariol <isabellagama2906@gmail.com>
2024-11-26 23:02:15 -0300

    feat: product store

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,6 +3,8 @@ import Search from "../../components/search";
 import { useAuth } from "../../context/auth-firebase";
 import CategoryNav from "../../components/category-nav";
 import CategoryStore from "../../components/category-store";
+import ProductStore from "../../components/product-store";
+import Footer from "../../components/footer";
 
 export default function Home() {
   const { user } = useAuth();
@@ -10,6 +12,7 @@ export default function Home() {
   return (
     <main className="home">
       <Header />
+      
       <section className="user-info mt-6 mb-8 pl-4 pr-4">
         <p className="text-secondary text-base">
           Hi, {user?.displayName || "User"}
@@ -20,10 +23,13 @@ export default function Home() {
         <Search />
       </section>
 
-      <section className="bg-gray-100 rounded-t-3xl mt-6 mb-8 pl-4 pr-4 pt-6 pb-6">
+      <section className="bg-gray-100 rounded-t-3xl mt-6 mb-8 pl-4 pr-4 pt-6 pb-12">
         <CategoryNav />
         <CategoryStore />
+        <ProductStore title="Recent products" />
       </section>
+    
+      <Footer />
     </main>
   );
 }
